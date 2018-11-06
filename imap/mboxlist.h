cdd51455ce9f89c58c7fe786bcbe3bb59bec0a98
Ken Murchison <murch@fastmail.com>
2018-11-06 15:15:58 -0500

    mboxlist.c: keep a history of mailbox names corresponding to a given uniqueid

--- imap/mboxlist.h
+++ imap/mboxlist.h
@@ -114,6 +114,8 @@ struct mboxlist_entry {
     char *uniqueid;
     /* legacy upgrade support */
     char *legacy_specialuse;
+    /* replication support */
+    struct dlist *synonyms;
 };
 
 typedef struct mboxlist_entry mbentry_t;
